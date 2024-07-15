2935cf78ceb31aa1504e8f90ffc1a0c8e0a925d3
PiHi <140247606+IAmPiHi@users.noreply.github.com>
2024-07-15 19:09:46 +0800

    Update allpost.js

--- allpost.js
+++ allpost.js
@@ -2,6 +2,6 @@ const blogs = [
     { title: "MySQL資料庫 語法筆記", link: "https://sites.google.com/view/zhangcode-database/mysql%E8%B3%87%E6%96%99%E5%BA%AB%E9%81%8B%E7%94%A8?authuser=2", desc: "MySQL基本語法，如果零基礎請先觀看教學影片。<br>如果已經有基礎觀念，該文章可以幫助快速回想 複習語法!", img: "bp/blog1.jpg", category: 'none' },
     {title: "C++指標", link: "https://sites.google.com/view/zhangcode-cppptr/c-%E6%8C%87%E6%A8%99?authuser=2", desc: "以資料結構linkedlist為範例，進行 C++指標的概念和運用教學。", img: "bp/blog2.jpg", category: 'cpp' },
     {title: "教學:如何用Linux寫程式?", link: "https://sites.google.com/view/zhangcodelinuxcoding/linux?authuser=2", desc: "Linux受益於類UNIX系統強大的終端和開源特性，深受眾多開發者的喜愛。<br>記得我第一次使用Linux寫程式時，也被它的強大和靈活性所驚豔。<br>掌握Linux無疑將在未來的道路上帶來巨大的幫助！", img: "bp/blog3.jpg", category: 'none' }//,{},{}
-        {title: "APCS2017-0304-3數字龍捲風", link: "https://sites.google.com/view/apcs2017-0304-3/%E9%A6%96%E9%A0%81?authuser=1", desc: "APCS2017-0304-3數字龍捲風。 AC解答", img: "bp/apcs.jpg", category: 'cpp' },
+        ,{title: "APCS2017-0304-3數字龍捲風", link: "https://sites.google.com/view/apcs2017-0304-3/%E9%A6%96%E9%A0%81?authuser=1", desc: "APCS2017-0304-3數字龍捲風。 AC解答", img: "bp/apcs.jpg", category: 'cpp' },
     // 添加更多網誌數據 cpp csharp none python
 ];
