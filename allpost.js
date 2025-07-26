239526d05252c793e57a4bbc8f9c159007e85316
PiHi <140247606+IAmPiHi@users.noreply.github.com>
2025-07-26 16:39:19 +0800

    Add files via upload

--- allpost.js
+++ allpost.js
@@ -3,5 +3,7 @@ const blogs = [
     {title: "C++指標", link: "https://sites.google.com/view/zhangcode-cppptr/c-%E6%8C%87%E6%A8%99?authuser=2", desc: "以資料結構linkedlist為範例，進行 C++指標的概念和運用教學。", img: "bp/blog2.jpg", category: 'cpp' },
     {title: "教學:如何用Linux寫程式?", link: "https://sites.google.com/view/zhangcodelinuxcoding/linux?authuser=2", desc: "Linux受益於類UNIX系統強大的終端和開源特性，深受眾多開發者的喜愛。<br>記得我第一次使用Linux寫程式時，也被它的強大和靈活性所驚豔。<br>掌握Linux無疑將在未來的道路上帶來巨大的幫助！", img: "bp/blog3.jpg", category: 'none' }//,{},{}
         ,{title: "APCS2017-0304-3數字龍捲風", link: "https://sites.google.com/view/apcs2017-0304-3/%E9%A6%96%E9%A0%81?authuser=1", desc: "APCS2017-0304-3數字龍捲風。 AC解答", img: "bp/apcs.jpg", category: 'cpp' },
-    {title: "C++超基礎教學(未完成)", link: "https://sites.google.com/view/cppeasylearning/%E9%A6%96%E9%A0%81?authuser=1", desc: "提供給完全沒接觸過C++ 甚至程式語言的人一個入門的教學文章<br> 幫助讀者快速理解進入狀況。", img: "bp/cppt.jpg", category: 'cpp' }// 添加更多網誌數據 cpp csharp none python
+    {title: "C++超基礎教學(未完成)", link: "https://sites.google.com/view/cppeasylearning/%E9%A6%96%E9%A0%81?authuser=1", desc: "提供給完全沒接觸過C++ 甚至程式語言的人一個入門的教學文章<br> 幫助讀者快速理解進入狀況。", img: "bp/cppt.jpg", category: 'cpp' }
+    ,
+    {title: "了解Computer Science能大幅提升程式能力?", link: "posts/others001.html", desc: "其實說白了，打程式就是在請電腦做某些特定的事情。但其中本來就有很多動作是牽涉到到腦運行的底層邏輯，例如C++的指標、或是你想繪製圖表(Bitmap)等等 都是Computer Science涵蓋的範圍。", img: "bp/CS.jpg", category: 'none' }// 添加更多網誌數據 cpp csharp none python
 ];
