67fd836de1136124635a510edc8623c3a7bb59aa
PiHi <140247606+IAmPiHi@users.noreply.github.com>
2025-08-05 22:39:57 +0800

    Add files via upload

--- allpost.js
+++ allpost.js
@@ -1,5 +1,5 @@
 const blogs = [
-    { title: "MySQL資料庫 語法筆記", link: "https://sites.google.com/view/zhangcode-database/mysql%E8%B3%87%E6%96%99%E5%BA%AB%E9%81%8B%E7%94%A8?authuser=2", desc: "MySQL基本語法，如果零基礎請先觀看教學影片。<br>如果已經有基礎觀念，該文章可以幫助快速回想 複習語法!", img: "bp/blog1.jpg", category: 'none' },
+    
     {title: "C++指標", link: "https://sites.google.com/view/zhangcode-cppptr/c-%E6%8C%87%E6%A8%99?authuser=2", desc: "以資料結構linkedlist為範例，進行 C++指標的概念和運用教學。", img: "bp/blog2.jpg", category: 'cpp' },
     {title: "教學:如何用Linux寫程式?", link: "https://sites.google.com/view/zhangcodelinuxcoding/linux?authuser=2", desc: "Linux受益於類UNIX系統強大的終端和開源特性，深受眾多開發者的喜愛。<br>記得我第一次使用Linux寫程式時，也被它的強大和靈活性所驚豔。<br>掌握Linux無疑將在未來的道路上帶來巨大的幫助！", img: "bp/blog3.jpg", category: 'none' }//,{},{}
         ,{title: "APCS2017-0304-3數字龍捲風", link: "https://sites.google.com/view/apcs2017-0304-3/%E9%A6%96%E9%A0%81?authuser=1", desc: "APCS2017-0304-3數字龍捲風。 AC解答", img: "bp/apcs.jpg", category: 'cpp' },
@@ -7,5 +7,8 @@ const blogs = [
     ,
     {title: "了解Computer Science能大幅提升程式能力?", link: "posts/others001.html", desc: "其實說白了，打程式就是在請電腦做某些特定的事情。但其中本來就有很多動作是牽涉到到腦運行的底層邏輯，例如C++的指標、或是你想繪製圖表(Bitmap)等等 都是Computer Science涵蓋的範圍。", img: "bp/CS.jpg", category: 'none' }
     ,
-    {title: "Flask vs Django: What are their strengths?", link: "posts/python001.html", desc: "Python 最well-known的兩個後端，肯定就是他們兩個。那他們兩個有啥不同，各自有啥優劣勢呢~~? 今天這篇文章就是來幫助大家區分兩者的優劣勢!", img: "bp/DVF.png", category: 'python' }// 添加更多網誌數據 cpp csharp none python
+    {title: "Flask vs Django: What are their strengths?", link: "posts/python001.html", desc: "Python 最well-known的兩個後端，肯定就是他們兩個。那他們兩個有啥不同，各自有啥優劣勢呢~~? 今天這篇文章就是來幫助大家區分兩者的優劣勢!", img: "bp/DVF.png", category: 'python' }
+    ,
+    { title: "MySQL 筆記 for beginner", link: "posts/others002.html", desc: "你一定常常聽到資料庫這個詞，現代網站和很多遊戲、軟體等等 後面都會有資料庫在管理各式各樣的資料。可以說是非常重要的一個東西，如果學會使用資料庫也可以大大增加自己的就業機會!", img: "bp/blog1.jpg", category: 'none' }
+    // 添加更多網誌數據 cpp csharp none python
 ];
