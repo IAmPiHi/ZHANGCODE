b18ea858e08b4069083a5e004e280920d09b3ee6
ChrisZhang <140247606+IAmPiHi@users.noreply.github.com>
2026-05-27 14:16:58 +0800

    Add files via upload

--- allpost.js
+++ allpost.js
@@ -2,7 +2,7 @@ const blogs = [
 
     {title: "C++指標", link: "https://sites.google.com/view/zhangcode-cppptr/c-%E6%8C%87%E6%A8%99?authuser=2", desc: "以資料結構linkedlist為範例，進行 C++指標的概念和運用教學。", img: "bp/blog2.jpg", category: 'programming' },
     {title: "教學:如何用Linux寫程式?", link: "https://sites.google.com/view/zhangcodelinuxcoding/linux?authuser=2", desc: "Linux受益於類UNIX系統強大的終端和開源特性，深受眾多開發者的喜愛。記得我第一次使用Linux寫程式時，也被它的強大和靈活性所驚豔。掌握Linux無疑將在未來的道路上帶來巨大的幫助！", img: "bp/blog3.jpg", category: 'tools' }//,{},{}
-        ,{title: "APCS2017-0304-3數字龍捲風", link: "https://sites.google.com/view/apcs2017-0304-3/%E9%A6%96%E9%A0%81?authuser=1", desc: "APCS2017-0304-3數字龍捲風。 AC解答", img: "bp/apcs.jpg", category: 'cs' },
+        ,{title: "APCS2017-0304-3數字龍捲風", link: "https://sites.google.com/view/apcs2017-0304-3/%E9%A6%96%E9%A0%81?authuser=1", desc: "APCS2017-0304-3數字龍捲風。 AC解答", img: "bp/apcs.jpg", category: 'programming' },
     {title: "C++超基礎教學(未完成)", link: "https://sites.google.com/view/cppeasylearning/%E9%A6%96%E9%A0%81?authuser=1", desc: "提供給完全沒接觸過C++ 甚至程式語言的人一個入門的教學文章 幫助讀者快速理解進入狀況。", img: "bp/cppt.jpg", category: 'programming' }
     ,
     {title: "了解Computer Science能大幅提升程式能力?", link: "posts/others001.html", desc: "其實說白了，打程式就是在請電腦做某些特定的事情。但其中本來就有很多動作是牽涉到到腦運行的底層邏輯，例如C++的指標、或是你想繪製圖表(Bitmap)等等 都是Computer Science涵蓋的範圍。", img: "bp/CS.jpg", category: 'cs' }
@@ -13,8 +13,10 @@ const blogs = [
     ,
     { title: ".dll 是什麼檔案？帶你了解 GCC 編譯器運作原理！", link: "posts/others003.html", desc: "這篇文章用淺顯方式講解 GCC 編譯流程，從預處理到連結都拆解給你看，也說明 .so/.dll 怎麼生成、動態/靜態連結差別，以及教學如何跨語言呼叫 C/C++ 函式/功能!", img: "bp/others003.png", category: 'programming' }
     ,
-    { title: "AI基礎理論知識 解密YT演算法!", link: "posts/others004.html", desc: "現在人工智慧是非常火熱的話題，今天就來帶大家簡單的認識一下AI吧~ 內容包括:簡單線性問題，到複雜神經網路 運作原理介紹。深入淺出帶大家認識AI!!!", img: "bp/others004.png", category: 'other' }
+    { title: "AI基礎理論知識 解密YT演算法!", link: "posts/others004.html", desc: "現在人工智慧是非常火熱的話題，今天就來帶大家簡單的認識一下AI吧~ 內容包括:簡單線性問題，到複雜神經網路 運作原理介紹。深入淺出帶大家認識AI!!!", img: "bp/others004.png", category: 'cs' }
     ,
     { title: "封包的旅程(OSI七層架構)", link: "posts/others005.html", desc: "我們常常在網站上按下\"送出\"後資料就神奇送到對方那裡，但過程封包究竟發生了什麼事?在這幾毫秒之間封包送過去又送回來，中間到底有什麼神奇的秘密?今天就來稍微介紹一下!", img: "bp/others005.png", category: 'cs' }
+        ,
+    {title: "AI時代衝擊之下該如何站穩腳步?", link: "posts/others006.html", desc: "在AI的衝擊之下，軟體設計的門檻一直被一再降低。我們該做什麼準備來保全自己的不可取代性? 今天就來和大家分享我的淺見，大家可以在看完後一起思考看看~", img: "bp/aigen.jpg", category: 'other' }
     // 添加更多網誌數據 cpp csharp none python
 ];
